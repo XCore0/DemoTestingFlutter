4795a1c5c484b77d01375d4fe1b7be345052a6c8
1HITCOMUNITY <1hitcomunitygt@gmail.com>
2026-09-02 10:06:55 +0700

    updated fiky folder

--- lib/Fikih/number.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-
-  int number1 = 10;
-  double number2 = 20.5;
-
-  print(number1);
-  print(number2);
-  
-}
---
+++ lib/Fiky/final.dart
@@ -0,0 +1,14 @@
+void main() {
+
+ var firstName ="edo";
+ var lastName ="rian";
+
+ var firstName1 = "monyet";
+ var lastName1 = "beruang";
+
+ print(firstName);
+ print(lastName);
+ print(firstName1);
+ print(lastName1);
+  
+}
